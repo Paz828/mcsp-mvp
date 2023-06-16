5a58aab98afb66210b0b3e9d6d4666e7f840a59d
Dorion Boesch <dorion.boesch@gmail.com>
2023-06-16 15:08:03 -0500

    more changes

--- public/main.js
+++ public/main.js
@@ -2,8 +2,8 @@ const createPage = async () => {
   const partyTable = document.querySelector("#party-table");
   try {
     const response = await fetch("https://encounter-gen.onrender.com/party");
-    const charArr = response.value.json();
-    console.log(response);
+    // const charArr = response.value.json();
+    console.log(response.json());
   } catch (err) {
     console.error(err);
   }
