c7e8bc6f95146f34983a499f150f8f98a6017a7a
Dorion Boesch <dorion.boesch@gmail.com>
2023-06-17 18:52:11 -0500

    added the creature endpoints to the server

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.get("/party/:id", async (req, res) => {
 
 //CREATE one
 app.post("/party", async (req, res) => {
-  const { char_lvl, char_name, char_class, char_ancestry } = req.body;
+  const { creature_lvl, char_name, char_class, char_ancestry } = req.body;
 
   try {
     const result = await pool.query(
@@ -103,6 +103,92 @@ app.put("/party/:id", async (req, res) => {
 });
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // creatures table enpoints
+
+// GET All
+app.get("/creature", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM creatures");
+    res.status(200).json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// GET One
+app.get("/creature/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "SELECT * FROM creatures WHERE creature_id = $1",
+      [id]
+    );
+    res.json(result.rows[0]).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//CREATE one
+app.post("/creature", async (req, res) => {
+  const { creature_lvl, creature_name, creature_plane, creature_url } =
+    req.body;
+
+  try {
+    const result = await pool.query(
+      "INSERT INTO creature (creature_lvl, creature_name, creature_plane, creature_url) VALUES ($1, $2, $3, $4) RETURNING *",
+      [creature_lvl, creature_name, creature_plane, creature_url]
+    );
+    res.status(201).send(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.send("Internal Server Error").status(500);
+  }
+});
+
+//DELETE one
+app.delete("/creature/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "DELETE FROM creature WHERE creature_id = $1 RETURNING *",
+      [id]
+    );
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.send("Internal Server Error").status(500);
+  }
+});
+//UPDATE one
+app.put("/creature/:id", async (req, res) => {
+  const { id } = req.params;
+  let input = "";
+
+  const bodyArr = Object.keys(req.body);
+  bodyArr.forEach((elem) => {
+    if (typeof req.body[elem] === "string") {
+      req.body[elem] = `'${req.body[elem]}'`;
+    }
+    if (!input) {
+      input += `${elem} = ${req.body[elem]}`;
+    } else {
+      input += `, ${elem} = ${req.body[elem]}`;
+    }
+  });
+
+  const SQLcode = `UPDATE creatures SET ${input} WHERE creature_id = ${id} RETURNING *;`;
+  try {
+    const result = await pool.query(SQLcode);
+    res.status(200).send(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Servor Error");
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
